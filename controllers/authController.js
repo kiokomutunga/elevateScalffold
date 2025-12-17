4b7ea5e02520f772a08c0a8c716bb6c3924439ba
kiokomutunga <kiokomutungapp@gmail.com>
2025-12-18 00:40:20 +0300

    otp creation for the authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,69 +1,21 @@
 import User from "../models/user.js";
 import otp from "../models/Otp.js";
 import bcrypt from "bcryptjs";
+const jwt = require("jsonwebtoken");
 
-const generateotp = () =>
-  Math.floor(100000 + Math.random() * 900000).toString();
+const generateAndSendOtp = async () => {
+    const otpCode = Math.floor(100000 + Math.random() * 900000).toString();
 
-//scale a number with six digit 
-
-//signing for the first time 
-
-export const signup = (req,res) => {
-  try{
-
-    const {name , email, password, adminCode } = req.body;
-    //destract to variables to work o them
-
-    if (adminCode !== process.env.ADMIN_ACCESS_CODE) {
-      return res.status(403).json({
-        message: "Ivalid Admin code Access Denied"
-      });
-    }
-
-    const existingUser = await User.findOne({email});
-    if(existingUser) {
-      return res.status(400).json({
-        message: "User already exists please loggin to continue"
-      });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    await User.create(
-      {
-        name,
-        email,
-        password: hashedPassword,
-        authProvider: "local",
-        isVerified: false
-      }
+    await Otp.findOneAndUpdate(
+        {email},
+        {code:otpCode, expiresAt: Date.now() + 10*60*1000},
+        {upsert: true}
     );
 
-    await otp.deleteMany ({email, purpose: "verify"});
-
-    const otpcode = generateOtp();
-
-    await otp.create({
-      email,
-      code: otpcode,
-      purpose: "verify",
-      expiresAt: new Date(Date.now() + 10* 60 * 1000)
-
-    });
-
-    //send email via email
-
-    return res.status(201).json ({
-      message: "Account created . Verify email sent to your emial"
-    });
-
-
-  }
-  catch(error){
-    console.error(error);
-    res.status(500).json({
-      message: "Account creation Failed"
-    })
-  }
+    return otpCode;
 };
+
+//generate the otp and assign the time until it expires
+
+//new user registering
+
